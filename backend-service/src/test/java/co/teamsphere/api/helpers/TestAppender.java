bf6f80f0d8ea732bb1094e908bf66659e29e645d
bravin <bravinrutoh@gmail.com>
2025-06-06 21:21:45 -0400

    CHAT-100: to remove custom getters and use lombok.

--- backend-service/src/test/java/co/teamsphere/api/helpers/TestAppender.java
+++ backend-service/src/test/java/co/teamsphere/api/helpers/TestAppender.java
@@ -2,20 +2,18 @@ package co.teamsphere.api.helpers;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Getter
 public class TestAppender extends AppenderBase<ILoggingEvent> {
     private final List<ILoggingEvent> logEvents = new CopyOnWriteArrayList<>();
 
     @Override
     protected void append(ILoggingEvent eventObject) {
         logEvents.add(eventObject);
-    }
-
-    public List<ILoggingEvent> getLogEvents() {
-        return logEvents;
     }
 
     public void clearEvents() {
