bf242843a0a871e2006472c84754566369c9bd4d
Toye.py <sobayotoye482@gmail.com>
2025-06-08 18:28:07 -0400

    Setup teamsphere upload bucket within the cdk stack

--- aws/src/main/java/com/myorg/AwsStack.java
+++ aws/src/main/java/com/myorg/AwsStack.java
@@ -3,6 +3,9 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketProps;
+import software.amazon.awscdk.RemovalPolicy;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 
@@ -14,7 +17,18 @@ public class AwsStack extends Stack {
     public AwsStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        // Clarify any assumptions concerning the bucket
+        Bucket uploadBucket = new Bucket(this, "TeamSphereUploadBucket", BucketProps.builder()
+            .bucketName("teamsphere-upload-bucket")
+            .versioned(true)
+            .removalPolicy(RemovalPolicy.DESTROY)
+            .autoDeleteObjects(true)
+            .build()
+        );
+
         // The code that defines your stack goes here
+
+
 
         // example resource
         // final Queue queue = Queue.Builder.create(this, "AwsQueue")
